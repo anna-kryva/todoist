91d715d5d0e43a17bdf56c7f9c723018591fd89d
Anna Kryva <anna120100@gmail.com>
2020-05-09 01:54:26 +0300

    Add environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCUtUM29Nq98XGeMf0NezjN-aruHF6gsyk'
 };
